846e30861c7ae6d1fd41323c9b76aa7a3b722f66
kwek20 <kwek20@users.noreply.github.com>
2023-12-10 01:09:05 +0000

    deploy: 59be9ba709777334b8bb0211c9103906df0ed38a

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,8 +5,8 @@ const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"manifest.json": "cc4b6aa791018840b65fd0b0e325b201",
 "favicon.png": "37d87985849bc680fe47a9330c3ea67e",
-"index.html": "df2b7a77bcd1a93043cfd6c2e6a5149f",
-"/": "df2b7a77bcd1a93043cfd6c2e6a5149f",
+"index.html": "54d3c6a7d56f32f76c3ed86769607594",
+"/": "54d3c6a7d56f32f76c3ed86769607594",
 "main.dart.js": "a2ea5b81219ed030d6df74a3db505fb1",
 "assets/FontManifest.json": "8c94f1ae76d43a46d6df5998e33f817d",
 "assets/AssetManifest.json": "13d2b287d24ff6e10afbd8e269bab4fd",
