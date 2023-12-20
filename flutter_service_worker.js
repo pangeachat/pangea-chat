909c57542baeeef27cfee95aba30e524010da067
kwek20 <kwek20@users.noreply.github.com>
2023-12-20 00:09:10 +0000

    deploy: 7dc2136034709ad876229cff0c1f25c9a4b1b2b9

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,8 +5,8 @@ const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"flutter.js": "7d69e653079438abfbb24b82a655b0a4",
 "manifest.json": "cc4b6aa791018840b65fd0b0e325b201",
-"index.html": "a008eefc14125c3335fc223cdfd01dfd",
-"/": "a008eefc14125c3335fc223cdfd01dfd",
+"index.html": "e81ef115b5a5394a1bcdb3fb791eed74",
+"/": "e81ef115b5a5394a1bcdb3fb791eed74",
 "assets/AssetManifest.bin": "880c7558f0fe7936f740f1fe37200f79",
 "assets/fonts/NotoEmoji/NotoColorEmoji.ttf": "ed84f46d3d5564a08541cd64bddd495c",
 "assets/fonts/Inconsolata/Inconsolata-Light.ttf": "aff8f4d4930b9e50eae584d911c309a4",
