8fecd6f7fef05492b84810a65ac5f26ae25224d8
Brord van Wierst <brord@iota.org>
2023-12-20 02:55:27 +0100

    hm9

--- scripts/upload-sentry.sh
+++ scripts/upload-sentry.sh
@@ -3,6 +3,8 @@
 # Build a release version of the app for a platform and upload symbols
 export OUTPUT_FOLDER_WEB=./build/web/
 export SENTRY_RELEASE=$CI_COMMIT_SHA
+export SENTRY_PROJECT="${SENTRY_ORG:-client}"
+export SENTRY_ORG="${SENTRY_ORG:-pangea-chat}"
 
 echo "[run] Uploading sourcemaps for $SENTRY_RELEASE"
 echo "[run] $SENTRY_PROJECT @ $SENTRY_ORG / $OUTPUT_FOLDER_WEB"
