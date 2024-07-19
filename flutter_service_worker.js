45fdd61912ae382494f6c2e251828921141c6e0f
ggurdin <ggurdin@users.noreply.github.com>
2024-07-19 19:12:37 +0000

    deploy: bd9b9b9f487965f1e00e1dc4c8d196836a60e735

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -17,7 +17,7 @@ const RESOURCES = {"icons/Icon-512.png": "f57dad4f6efa0339b50d5c65f36dc03c",
 "canvaskit/skwasm.js": "5d4f9263ec93efeb022bb14a3881d240",
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "e94cd312cb23accac577ffd6fd1da1ac",
+"flutter_bootstrap.js": "ae206214e2322e7580be5f22ca40c7b6",
 "version.json": "0d909939b62712014480ae2f9d3378a9",
 "auth.html": "88530dca48290678d3ce28a34fc66cbd",
 "favicon.png": "37d87985849bc680fe47a9330c3ea67e",
@@ -101,9 +101,9 @@ const RESOURCES = {"icons/Icon-512.png": "f57dad4f6efa0339b50d5c65f36dc03c",
 "splash/img/light-4x.png": "ead87864be6b8f2f3efbb04acd30549d",
 "splash/img/light-1x.png": "92fff8efa59621bf2b218b65a7f64014",
 "splash/img/dark-4x.png": "ead87864be6b8f2f3efbb04acd30549d",
-"index.html": "69bb93b388fbe20ae5218f7d25f16f2b",
-"/": "69bb93b388fbe20ae5218f7d25f16f2b",
-"main.dart.js": "2b5c96050a934d657961c4d4589de9ce"};
+"index.html": "72d217aebd404226a1d7e0b1eaf22e72",
+"/": "72d217aebd404226a1d7e0b1eaf22e72",
+"main.dart.js": "0e5b950c6d4c3c96114ecb093e198d3f"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
