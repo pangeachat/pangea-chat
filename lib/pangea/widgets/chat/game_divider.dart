dc70a385959fd7ac22314742a187d2fd8c15ddb6
Kelrap <kel.raphael3@outlook.com>
2024-08-14 17:19:21 -0400

    Make divider better

--- lib/pangea/widgets/chat/game_divider.dart
+++ lib/pangea/widgets/chat/game_divider.dart
@@ -1,7 +1,9 @@
 import 'package:fluffychat/config/app_config.dart';
 import 'package:fluffychat/pages/chat/chat.dart';
+import 'package:fluffychat/pangea/models/games/game_state_model.dart';
 import 'package:fluffychat/pangea/widgets/chat/round_timer.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/l10n.dart';
 import 'package:matrix/matrix.dart';
 
 class GameDivider extends StatelessWidget {
@@ -12,53 +14,60 @@ class GameDivider extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final currentCharacter =
-        controller.currentRound?.gameState.currentCharacter;
-    // if (currentCharacter == null) {
-    //   return const SizedBox();
-    // }
-    final startTime = controller.currentRound?.gameState.currentRoundStartTime;
+    final gameState = controller.currentRound?.gameState;
+    final eventState = GameModel.fromJson(event.content);
+    // Only show most recent divider
+    // Check that event time == current round start/end time
+    if ((eventState.previousRoundEndTime == null)
+        ? (eventState.currentRoundStartTime != gameState?.currentRoundStartTime)
+        : (eventState.previousRoundEndTime !=
+            gameState?.previousRoundEndTime)) {
+      return const SizedBox();
+    }
+    // Don't show if there is no current character
+    if (gameState?.currentCharacter == null) {
+      return const SizedBox();
+    }
 
-    final textColor = Theme.of(context).colorScheme.onSurface;
     final color = Theme.of(context).colorScheme.surfaceContainerHighest;
 
-    return Container(
-      alignment: Alignment.center,
-      padding: const EdgeInsets.all(16),
-      child: Material(
-        color: color,
-        clipBehavior: Clip.antiAlias,
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(
-            Radius.circular(AppConfig.borderRadius),
-          ),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(
-            16,
-          ),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text(
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          alignment: Alignment.center,
+          padding: const EdgeInsets.all(16),
+          child: Material(
+            color: color,
+            clipBehavior: Clip.antiAlias,
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(AppConfig.borderRadius),
+              ),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(
+                16,
+              ),
+              child: Text(
                 // TODO: use L10n instead of hardcoding this
-                currentCharacter == null
-                    ? "No character, sorry"
-                    : "$currentCharacter says...",
+                L10n.of(context)!.currentCharDialoguePrompt(
+                  gameState!.currentCharacter!,
+                ),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 20 * AppConfig.fontSizeFactor,
                 ),
               ),
-              const SizedBox(
-                height: 6,
-              ),
-              // if (startTime != null)
-              RoundTimer(controller: controller),
-            ],
+            ),
           ),
         ),
-      ),
+        // if (startTime != null)
+        RoundTimer(controller: controller),
+        const SizedBox(
+          height: 9,
+        ),
+      ],
     );
   }
 }
