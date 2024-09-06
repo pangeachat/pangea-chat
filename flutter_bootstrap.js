61929deb4ee73cef3ea846990cd3622bbff72f8f
ggurdin <ggurdin@users.noreply.github.com>
2024-09-06 16:33:57 +0000

    deploy: 25c1adff729224459ffc8f9ff11fbf3fbafaaa07

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a6bd3f1de158bb61090e0c8053df93a10cb548
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3906314807"
+    serviceWorkerVersion: "2099629075"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,9 +3,9 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"main.dart.js": "87ff0b27e2c91a067a41d57506983211",
-"index.html": "6ed71b63be37100eaa657b53a1c8609c",
-"/": "6ed71b63be37100eaa657b53a1c8609c",
+const RESOURCES = {"main.dart.js": "be584edd430334c417e9c85553c0de16",
+"index.html": "52d7b2438c79b92a2fa2c9816398fb40",
+"/": "52d7b2438c79b92a2fa2c9816398fb40",
 "splash/splash.js": "c6a271349a0cd249bdb6d3c4d12f5dcf",
 "splash/style.css": "adebf1d2354fbed5890fff78eac514ee",
 "splash/img/dark-4x.png": "ead87864be6b8f2f3efbb04acd30549d",
@@ -91,7 +91,7 @@ const RESOURCES = {"main.dart.js": "87ff0b27e2c91a067a41d57506983211",
 "assets/packages/flutter_math_fork/lib/katex_fonts/fonts/KaTeX_Fraktur-Bold.ttf": "46b41c4de7a936d099575185a94855c4",
 "assets/AssetManifest.bin.json": "6d1607693a55d6bb3ffa6a96159be53b",
 "assets/AssetManifest.bin": "9bf068346b53420f3eabfc8e0e7cd472",
-"flutter_bootstrap.js": "c34b29c0e88f47c4534315215710cd84",
+"flutter_bootstrap.js": "4ecb27fc7f61391c8773ba446fc5d789",
 "canvaskit/skwasm.js": "694fda5704053957c2594de355805228",
 "canvaskit/chromium/canvaskit.wasm": "b1ac05b29c127d86df4bcfbf50dd902a",
 "canvaskit/chromium/canvaskit.js": "671c6b4f8fcc199dcc551c7bb125f239",
