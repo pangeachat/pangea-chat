83763b59d35918e4533835457dad15ce2732f0ee
ggurdin <ggurdin@users.noreply.github.com>
2024-10-10 21:11:26 +0000

    deploy: 6c90d09a1a48762bca30b43d15d3a98464644bd1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -88,8 +88,8 @@ const RESOURCES = {"flutter.js": "f393d3c16b631f36852323de8e583132",
 "assets/AssetManifest.json": "f56a6de460f0ffdd6f9ac31712ac9197",
 "assets/AssetManifest.bin.json": "9969a9c61e12b6fae7f81133f15943ed",
 "auth.html": "88530dca48290678d3ce28a34fc66cbd",
-"index.html": "dab601fa47beacd14c1e5f365849000d",
-"/": "dab601fa47beacd14c1e5f365849000d",
+"index.html": "53cd5de84fdf927de11f0160d6edf305",
+"/": "53cd5de84fdf927de11f0160d6edf305",
 "manifest.json": "cc4b6aa791018840b65fd0b0e325b201",
 "canvaskit/canvaskit.js": "66177750aff65a66cb07bb44b8c6422b",
 "canvaskit/canvaskit.js.symbols": "48c83a2ce573d9692e8d970e288d75f7",
@@ -105,7 +105,7 @@ const RESOURCES = {"flutter.js": "f393d3c16b631f36852323de8e583132",
 "icons/Icon-512.png": "f57dad4f6efa0339b50d5c65f36dc03c",
 "favicon.png": "37d87985849bc680fe47a9330c3ea67e",
 "version.json": "dd39af0440ce4d301fa384db8377de88",
-"flutter_bootstrap.js": "8bf20e4c744243b5a29d024324d7bcfc"};
+"flutter_bootstrap.js": "1fc24573b1bb9b0253c56d109545f22e"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
