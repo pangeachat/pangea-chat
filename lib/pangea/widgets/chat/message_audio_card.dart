ddbab81281fe743d856e9453a9cced9ebe0d6a2e
William Jordan-Cooley <wcjord@gmail.com>
2024-10-17 14:33:03 -0400

    now using timestamps from text to speech results

--- lib/pangea/widgets/chat/message_audio_card.dart
+++ lib/pangea/widgets/chat/message_audio_card.dart
@@ -1,6 +1,7 @@
-import 'dart:math';
+import 'dart:developer';
 
 import 'package:fluffychat/pages/chat/events/audio_player.dart';
+import 'package:fluffychat/pangea/controllers/text_to_speech_controller.dart';
 import 'package:fluffychat/pangea/matrix_event_wrappers/pangea_message_event.dart';
 import 'package:fluffychat/pangea/models/pangea_token_model.dart';
 import 'package:fluffychat/pangea/utils/error_handler.dart';
@@ -8,8 +9,8 @@ import 'package:fluffychat/pangea/widgets/chat/message_selection_overlay.dart';
 import 'package:fluffychat/pangea/widgets/chat/message_toolbar.dart';
 import 'package:fluffychat/pangea/widgets/chat/toolbar_content_loading_indicator.dart';
 import 'package:fluffychat/pangea/widgets/igc/card_error_widget.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/l10n.dart';
 import 'package:matrix/matrix.dart';
 
 class MessageAudioCard extends StatefulWidget {
@@ -33,70 +34,12 @@ class MessageAudioCardState extends State<MessageAudioCard> {
   Event? localAudioEvent;
   PangeaAudioFile? audioFile;
 
-  Future<void> fetchAudio() async {
-    if (!mounted) return;
-    setState(() => _isLoading = true);
-
-    try {
-      final String langCode = widget.messageEvent.messageDisplayLangCode;
-      final String? text =
-          widget.messageEvent.representationByLanguage(langCode)?.text;
-      if (text != null) {
-        final Event? localEvent =
-            widget.messageEvent.getTextToSpeechLocal(langCode, text);
-        if (localEvent != null) {
-          localAudioEvent = localEvent;
-          if (mounted) setState(() => _isLoading = false);
-          return;
-        }
-      }
-
-      audioFile =
-          await widget.messageEvent.getMatrixAudioFile(langCode, context);
-      if (mounted) setState(() => _isLoading = false);
-    } catch (e, s) {
-      debugPrint(StackTrace.current.toString());
-      if (!mounted) return;
-      setState(() => _isLoading = false);
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text(L10n.of(context)!.errorGettingAudio),
-        ),
-      );
-      ErrorHandler.logError(
-        e: Exception(),
-        s: s,
-        m: 'something wrong getting audio in MessageAudioCardState',
-        data: {
-          'widget.messageEvent.messageDisplayLangCode':
-              widget.messageEvent.messageDisplayLangCode,
-        },
-      );
-    }
-    return;
-  }
-
-  final rnd = Random();
   double? sectionStartMS;
   double? sectionEndMS;
 
   @override
   void initState() {
-    if (widget.selection != null) {
-      sectionStartMS = rnd.nextDouble() * (1 - 0) + 0;
-      sectionEndMS = rnd.nextDouble() * (1 - 0) + 0;
-      if (sectionEndMS! < sectionStartMS!) {
-        final double temp = sectionEndMS!;
-        sectionEndMS = sectionStartMS;
-        sectionStartMS = temp;
-      }
-    }
     super.initState();
-
-    //once we have audio for words, we'll play that
-    if (widget.overlayController.isSelection) {
-      widget.overlayController.clearSelection();
-    }
 
     fetchAudio();
   }
@@ -105,21 +48,123 @@ class MessageAudioCardState extends State<MessageAudioCard> {
   void didUpdateWidget(covariant oldWidget) {
     if (oldWidget.selection != widget.selection) {
       debugPrint('selection changed');
-      if (widget.selection == null) {
-        sectionStartMS = null;
-        sectionEndMS = null;
-      } else {
-        sectionStartMS = rnd.nextDouble() * (1 - 0) + 0;
-        sectionEndMS = rnd.nextDouble() * (1 - 0) + 0;
-        if (sectionEndMS! < sectionStartMS!) {
-          final double temp = sectionEndMS!;
-          sectionEndMS = sectionStartMS;
-          sectionStartMS = temp;
-        }
-      }
-      setState(() {});
+      setSectionStartAndEndFromSelection();
     }
     super.didUpdateWidget(oldWidget);
+  }
+
+  void setSectionStartAndEnd(double? start, double? end) => mounted
+      ? setState(() {
+          sectionStartMS = start;
+          sectionEndMS = end;
+        })
+      : null;
+
+  void setSectionStartAndEndFromSelection() {
+    if (audioFile == null) {
+      // should never happen but just in case
+      debugger(when: kDebugMode);
+      return;
+    }
+
+    if (audioFile!.duration == null) {
+      // should never happen but just in case
+      debugger(when: kDebugMode);
+      ErrorHandler.logError(
+        e: Exception(),
+        m: 'audioFile duration is null in MessageAudioCardState',
+        data: {
+          'audioFile': audioFile,
+        },
+      );
+      return setSectionStartAndEnd(null, null);
+    }
+
+    // if there is no selection, we don't need to do anything
+    // but clear the section start and end
+    if (widget.selection == null) {
+      return setSectionStartAndEnd(null, null);
+    }
+
+    final PangeaTokenText selection = widget.selection!;
+    final List<TTSToken> tokens = audioFile!.tokens;
+
+    // find the token that corresponds to the selection
+    // set the start to the start of the token
+    // set the end to the start of the next token or to the duration of the audio if
+    // if there is no next token
+    for (int i = 0; i < tokens.length; i++) {
+      final TTSToken ttsToken = tokens[i];
+      if (ttsToken.text.offset == selection.offset) {
+        final double start = ttsToken.startMS.toDouble();
+        final double end = ttsToken.endMS.toDouble();
+        return setSectionStartAndEnd(start, end);
+      }
+    }
+
+    // if we didn't find the token, we should pause if debug and log an error
+    debugger(when: kDebugMode);
+    ErrorHandler.logError(
+      e: Exception(),
+      m: 'could not find token for selection in MessageAudioCardState',
+      data: {
+        'selection': selection,
+        'tokens': tokens,
+        'sttTokens': audioFile!.tokens,
+      },
+    );
+
+    setSectionStartAndEnd(null, null);
+  }
+
+  Future<void> fetchAudio() async {
+    if (!mounted) return;
+    setState(() => _isLoading = true);
+
+    // try {
+    final String langCode = widget.messageEvent.messageDisplayLangCode;
+    final String? text =
+        widget.messageEvent.representationByLanguage(langCode)?.text;
+
+    if (text != null) {
+      //TODO - handle error but get out of flow
+    }
+
+    final Event? localEvent =
+        widget.messageEvent.getTextToSpeechLocal(langCode, text!);
+
+    if (localEvent != null) {
+      debugger(when: kDebugMode);
+      localAudioEvent = localEvent;
+      // @ggurdin why don't we set audioFile here?
+      if (mounted) setState(() => _isLoading = false);
+      return;
+    }
+
+    audioFile = await widget.messageEvent.getMatrixAudioFile(langCode, context);
+
+    setSectionStartAndEndFromSelection();
+    // } catch (e, s) {
+    //   debugger(when: kDebugMode);
+    //   debugPrint(StackTrace.current.toString());
+    //   if (!mounted) return;
+    //   setState(() => _isLoading = false);
+    //   ScaffoldMessenger.of(context).showSnackBar(
+    //     SnackBar(
+    //       content: Text(L10n.of(context)!.errorGettingAudio),
+    //     ),
+    //   );
+    //   ErrorHandler.logError(
+    //     e: Exception(),
+    //     s: s,
+    //     m: 'something wrong getting audio in MessageAudioCardState',
+    //     data: {
+    //       'widget.messageEvent.messageDisplayLangCode':
+    //           widget.messageEvent.messageDisplayLangCode,
+    //     },
+    //   );
+    // }
+    return;
   }
 
   @override
@@ -149,6 +194,7 @@ class MessageAudioCardState extends State<MessageAudioCard> {
 
 class PangeaAudioFile extends MatrixAudioFile {
   List<int>? waveform;
+  List<TTSToken> tokens;
 
   PangeaAudioFile({
     required super.bytes,
@@ -156,5 +202,6 @@ class PangeaAudioFile extends MatrixAudioFile {
     super.mimeType,
     super.duration,
     this.waveform,
+    required this.tokens,
   });
 }
