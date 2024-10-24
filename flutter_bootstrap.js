7d4be8efeabde617310e4cae7f4656822624c99f
wcjord <wcjord@users.noreply.github.com>
2024-10-24 16:58:46 +0000

    deploy: 00d6277bc6cc1cbf4eb7d357f53d5a6ecdf3f3e1

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"c9b9d5780da342eb3f0f5e439a7db06f7d1125
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3571848095"
+    serviceWorkerVersion: "2794415903"
   }
 });
