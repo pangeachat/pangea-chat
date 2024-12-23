4f9775dfbe9ef3be99b81ec5d87cd7c52b000fa1
ggurdin <ggurdin@gmail.com>
2024-12-23 14:49:21 -0500

    Added queuing / UI logic for morph activity buttons

--- lib/pangea/widgets/word_zoom/morphological_widget.dart
+++ lib/pangea/widgets/word_zoom/morphological_widget.dart
@@ -7,17 +7,29 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:material_symbols_icons/symbols.dart';
 
+class ActivityMorph {
+  final String morphFeature;
+  final String morphTag;
+  bool revealed;
+
+  ActivityMorph({
+    required this.morphFeature,
+    required this.morphTag,
+    required this.revealed,
+  });
+}
+
 class MorphologicalListWidget extends StatefulWidget {
-  final String? selectedMorphFeature;
   final PangeaToken token;
-  final Function(String) onPressed;
+  final String? selectedMorphFeature;
+  final Function(String?) setMorphFeature;
   final int completedActivities;
 
   const MorphologicalListWidget({
     super.key,
     required this.selectedMorphFeature,
     required this.token,
-    required this.onPressed,
+    required this.setMorphFeature,
     required this.completedActivities,
   });
 
@@ -30,77 +42,163 @@ class MorphologicalListWidgetState extends State<MorphologicalListWidget> {
   // For each item in the sequence,
   //    if shouldDoActivity is true, show the template icon then stop
   //    if shouldDoActivity is false, show the actual icon and value then go to the next item
-  final Map<String, bool> _morphEnabledStatus = {};
+
+  final List<ActivityMorph> _morphs = [];
 
   @override
   void initState() {
     super.initState();
-    _setMorphEnabledStatus();
+    _setMorphs();
   }
 
   @override
   void didUpdateWidget(covariant MorphologicalListWidget oldWidget) {
     super.didUpdateWidget(oldWidget);
-    if (widget.token != oldWidget.token ||
-        widget.completedActivities != oldWidget.completedActivities) {
-      _setMorphEnabledStatus();
+    if (widget.token != oldWidget.token) {
+      _setMorphs();
+    }
+
+    if (widget.completedActivities != oldWidget.completedActivities &&
+        oldWidget.selectedMorphFeature != null) {
+      final oldSelectedMorphIndex = _morphs.indexWhere(
+        (morph) => morph.morphFeature == oldWidget.selectedMorphFeature,
+      );
+      if (oldSelectedMorphIndex != -1 &&
+          oldSelectedMorphIndex < _morphs.length) {
+        setState(
+          () => _morphs[oldSelectedMorphIndex].revealed = true,
+        );
+
+        final nextIndex = oldSelectedMorphIndex + 1;
+        if (nextIndex < _morphs.length) {
+          widget.setMorphFeature(_morphs[nextIndex].morphFeature);
+        } else {
+          widget.setMorphFeature(null);
+        }
+      }
+
+      // The previous morph activity has been completed, so move on to the next one
+      // if it's available. If not, de-selected the completed activity.
+      // if (widget.selectedMorphFeature == null) return;
+      // final nextSelectedIndex = _morphEnabledStatus.length - _revealedMorphs;
+
+      // if (nextSelectedIndex <=
+      //         _sortedMorphFeatures.indexOf(widget.selectedMorphFeature!) ||
+      //     nextSelectedIndex >= _morphEnabledStatus.length) {
+      //   widget.setMorphFeature(null);
+      // } else {
+      //   widget.setMorphFeature(_sortedMorphFeatures[nextSelectedIndex]);
+      // }
     }
   }
 
-  Future<void> _setMorphEnabledStatus() async {
-    _morphEnabledStatus.clear();
-    for (final entry in widget.token.morph.entries) {
-      final shouldDoActivity = widget.token.shouldDoMorphActivity(entry.key);
+  Future<void> _setMorphs() async {
+    _morphs.clear();
+    final morphEntries = widget.token.morph.entries.toList();
+    for (final morphEntry in morphEntries) {
+      final morphFeature = morphEntry.key;
+      final morphTag = morphEntry.value;
+      final shouldDoActivity = widget.token.shouldDoMorphActivity(morphFeature);
       final canGenerateDistractors = await widget.token.canGenerateDistractors(
         ActivityTypeEnum.morphId,
-        morphFeature: entry.key,
-        morphTag: entry.value,
+        morphFeature: morphFeature,
+        morphTag: morphTag,
       );
-      _morphEnabledStatus[entry.key] =
-          shouldDoActivity && canGenerateDistractors;
+      _morphs.add(
+        ActivityMorph(
+          morphFeature: morphFeature,
+          morphTag: morphTag,
+          revealed: !shouldDoActivity || !canGenerateDistractors,
+        ),
+      );
     }
-    if (mounted) setState(() {});
+
+    _morphs.sort((a, b) {
+      if (a.revealed && !b.revealed) {
+        return -1;
+      } else if (!a.revealed && b.revealed) {
+        return 1;
+      }
+
+      if (a.morphFeature.toLowerCase() == "pos") {
+        return -1;
+      } else if (b.morphFeature.toLowerCase() == "pos") {
+        return 1;
+      }
+
+      return a.morphFeature.compareTo(b.morphFeature);
+    });
   }
+
+  List<ActivityMorph> get _visibleMorphs {
+    final lastRevealedIndex = _morphs.lastIndexWhere((morph) => morph.revealed);
+    debugPrint("last revealed index: $lastRevealedIndex");
+
+    // if none of the morphs are revealed, show only the first one
+    if (lastRevealedIndex == -1) {
+      return _morphs.take(1).toList();
+    }
+
+    // show all the revealed morphs + the first one with an activity
+    return _morphs.take(lastRevealedIndex + 2).toList();
+  }
+
+  // Future<void> _setMorphEnabledStatus() async {
+  //   _morphEnabledStatus.clear();
+  //   for (final entry in widget.token.morph.entries) {
+  // final shouldDoActivity = widget.token.shouldDoMorphActivity(entry.key);
+  // final canGenerateDistractors = await widget.token.canGenerateDistractors(
+  //   ActivityTypeEnum.morphId,
+  //   morphFeature: entry.key,
+  //   morphTag: entry.value,
+  // );
+  //     _morphEnabledStatus[entry.key] =
+  //         shouldDoActivity && canGenerateDistractors;
+  //   }
+  //   if (mounted) setState(() {});
+  // }
 
   // TODO Use the icons that Khue is creating
   IconData _getIconForMorphFeature(String feature) {
     // Define a function to get the icon based on the universal dependency morphological feature (key)
-    switch (feature) {
-      case 'Number':
+    switch (feature.toLowerCase()) {
+      case 'number':
         // google material 123 icon
         return Icons.format_list_numbered;
-      case 'Gender':
+      case 'gender':
         return Icons.wc;
-      case 'Tense':
+      case 'tense':
         return Icons.access_time;
-      case 'Mood':
+      case 'mood':
         return Icons.mood;
-      case 'Person':
+      case 'person':
         return Icons.person;
-      case 'Case':
+      case 'case':
         return Icons.format_list_bulleted;
-      case 'Degree':
+      case 'degree':
         return Icons.trending_up;
-      case 'VerbForm':
+      case 'verbform':
         return Icons.text_format;
-      case 'Voice':
+      case 'voice':
         return Icons.record_voice_over;
-      case 'Aspect':
+      case 'aspect':
         return Icons.aspect_ratio;
-      case 'PronType':
+      case 'prontype':
         return Icons.text_fields;
-      case 'NumType':
+      case 'numtype':
         return Icons.format_list_numbered;
-      case 'Poss':
+      case 'poss':
         return Icons.account_balance;
-      case 'Reflex':
+      case 'reflex':
         return Icons.refresh;
-      case 'Foreign':
+      case 'foreign':
         return Icons.language;
-      case 'Abbr':
+      case 'abbr':
         return Icons.text_format;
-      case 'NounType':
+      case 'nountype':
         return Symbols.abc;
+      case 'pos':
+        return Symbols.toys_and_games;
       default:
         debugger(when: kDebugMode);
         return Icons.help_outline;
@@ -110,15 +208,15 @@ class MorphologicalListWidgetState extends State<MorphologicalListWidget> {
   @override
   Widget build(BuildContext context) {
     return Wrap(
-      children: widget.token.morph.entries.map((entry) {
+      children: _visibleMorphs.map((morph) {
         return Padding(
           padding: const EdgeInsets.all(2.0),
           child: MorphologicalActivityButton(
-            onPressed: widget.onPressed,
-            morphCategory: entry.key,
-            icon: _getIconForMorphFeature(entry.key),
-            isUnlocked: _morphEnabledStatus[entry.key] ?? false,
-            isSelected: widget.selectedMorphFeature == entry.key,
+            onPressed: widget.setMorphFeature,
+            morphCategory: morph.morphFeature,
+            icon: _getIconForMorphFeature(morph.morphFeature),
+            isUnlocked: morph.revealed,
+            isSelected: widget.selectedMorphFeature == morph.morphFeature,
           ),
         );
       }).toList(),
@@ -152,21 +250,13 @@ class MorphologicalActivityButton extends StatelessWidget {
             morphCategory,
             context,
           ),
-          child: OutlinedButton(
-            onPressed: () {
-              onPressed(morphCategory);
-            },
-            style: OutlinedButton.styleFrom(
-              side: BorderSide(
-                color: isSelected
-                    ? Colors.green
-                    : isUnlocked
-                        ? Colors.blue
-                        : Colors.grey,
-                width: 2,
-              ),
+          child: Opacity(
+            opacity: (isUnlocked && !isSelected) ? 0.75 : 1,
+            child: IconButton(
+              onPressed: () => onPressed(morphCategory),
+              icon: Icon(icon),
+              color: isSelected ? Theme.of(context).colorScheme.primary : null,
             ),
-            child: Icon(icon, size: 20),
           ),
         ),
       ],
