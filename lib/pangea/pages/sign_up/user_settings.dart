496fd019db4ec68f63d0999c7cc5c6e9e5342b13
ggurdin <ggurdin@gmail.com>
2024-12-30 10:43:59 -0500

    fix: if user signs up with SSO, make them agree to TOS on language/avatar settings page

--- lib/pangea/pages/sign_up/user_settings.dart
+++ lib/pangea/pages/sign_up/user_settings.dart
@@ -29,11 +29,14 @@ class UserSettingsState extends State<UserSettingsPage> {
 
   String? selectedLanguageError;
   String? profileCreationError;
+  String? tncError;
 
   bool loading = false;
 
   Uint8List? avatar;
   String? _selectedFilePath;
+
+  bool isTncChecked = false;
 
   List<String> avatarPaths = const [
     "assets/pangea/Avatar_1.png",
@@ -52,7 +55,6 @@ class UserSettingsState extends State<UserSettingsPage> {
         : PangeaLanguage.byLangCode(systemLangCode);
   }
 
-  bool canSetDisplayName = false;
   TextEditingController displayNameController = TextEditingController();
   final GlobalKey<FormState> formKey = GlobalKey<FormState>();
 
@@ -61,11 +63,6 @@ class UserSettingsState extends State<UserSettingsPage> {
     super.initState();
     selectedTargetLanguage = _pangeaController.languageController.userL2;
     selectedAvatarPath = avatarPaths.first;
-    final loginTypeEntry =
-        _pangeaController.pStoreService.read(PLocalKey.loginType);
-    if (loginTypeEntry is String && loginTypeEntry == 'sso') {
-      canSetDisplayName = true;
-    }
   }
 
   @override
@@ -74,7 +71,21 @@ class UserSettingsState extends State<UserSettingsPage> {
     loading = false;
     selectedLanguageError = null;
     profileCreationError = null;
+    tncError = null;
     super.dispose();
+  }
+
+  bool get isSSOSignup {
+    final loginTypeEntry =
+        _pangeaController.pStoreService.read(PLocalKey.loginType);
+    return loginTypeEntry is String && loginTypeEntry == 'sso';
+  }
+
+  void setTncChecked(bool? value) {
+    setState(() {
+      isTncChecked = value ?? false;
+      tncError = null;
+    });
   }
 
   void setSelectedTargetLanguage(LanguageModel? language) {
@@ -153,11 +164,22 @@ class UserSettingsState extends State<UserSettingsPage> {
   }
 
   Future<void> createUserInPangea() async {
-    setState(() => profileCreationError = null);
+    setState(() {
+      profileCreationError = null;
+      selectedLanguageError = null;
+      tncError = null;
+    });
 
     if (selectedTargetLanguage == null) {
       setState(() {
         selectedLanguageError = L10n.of(context).pleaseSelectALanguage;
+      });
+      return;
+    }
+
+    if (isSSOSignup && !isTncChecked) {
+      setState(() {
+        tncError = L10n.of(context).pleaseAgreeToTOS;
       });
       return;
     }
